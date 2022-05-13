e778feb9a0c99f2f7afded8bdd5cd1bfe26cfca3
ShainAliiev <shain.aliiev@gmail.com>
2022-05-13 16:07:40 -0400

    Final version

--- index.js
+++ index.js
@@ -15,78 +15,75 @@ const body = document.querySelector("body");
 //const div = document.createElement("div");
 
 let number = Math.floor(Math.random() * 5) + 1;
-console.log(number);
+let i = 20;
+let i1 = 20;
+
+check.addEventListener("click", function () {
+  //div.textContent = input1.value;
+  //document.querySelector("h3").appendChild(div);
+
+  if (Number(input1.value) === number) {
+    question.textContent = number;
+    high.textContent = "Player1 Wins!";
+    highscore.textContent = `High score-  ${i}`;
+    body.classList.add("bg");
+  } else if (Number(input1.value) > number) {
+    i--;
+    high.textContent = "Too high!";
+    pl1score.textContent = `Player1-  ${i} `;
+
+    setTimeout(() => {
+      let number2 = Math.floor(Math.random() * 5) + 1;
+      input2.textContent = number2;
+      if (number2 === number) {
+        high.textContent = "Player2 Wins!";
+        highscore.textContent = `High score- ${i1}`;
+        question.textContent = number;
+        body.classList.add("bg");
+      } else if (number2 > number) {
+        high.textContent = "Too high!";
+        i1--;
+        pl2score.textContent = `Player2- ${i1}`;
+      } else {
+        high.textContent = "Too low!";
+        i1--;
+        pl2score.textContent = `Player2- ${i1}`;
+      }
+    }, 2000);
+  } else {
+    i--;
+    high.textContent = "Too low!";
+    pl1score.textContent = `Player1-  ${i}`;
+    setTimeout(() => {
+      let number2 = Math.floor(Math.random() * 5) + 1;
+      input2.textContent = number2;
+      if (number2 === number) {
+        high.textContent = "Player2 Wins!";
+        highscore.textContent = `High score- ${i1}`;
+        question.textContent = number;
+        body.classList.add("bg");
+      } else if (number2 > number) {
+        high.textContent = "Too high!";
+        i1--;
+        pl2score.textContent = `Player2- ${i1}`;
+      } else {
+        high.textContent = "Too low!";
+        i1--;
+        pl2score.textContent = `Player2- ${i1}`;
+      }
+    }, 2000);
+  }
+});
 
 again.addEventListener("click", function () {
-  "";
-
-  let number = Math.floor(Math.random() * 5) + 1;
-
+  i = 20;
+  i1 = 20;
+  number = Math.floor(Math.random() * 5) + 1;
   body.classList.remove("bg");
   question.textContent = "?";
-  pl1score.textContent = "Player1-  20";
-  pl2score.textContent = "Player1-  20";
+  pl1score.textContent = `Player1-  ${i}`;
+  pl2score.textContent = `Player1-  ${i1}`;
   input1.value = "";
   input2.textContent = "";
   console.log(number);
-
-  let i = 20;
-  let i1 = 20;
-
-  check.addEventListener("click", function () {
-    //div.textContent = input1.value;
-    //document.querySelector("h3").appendChild(div);
-
-    if (Number(input1.value) === number) {
-      question.textContent = number;
-      high.textContent = "Player1 Wins!";
-      highscore.textContent = `High score-  ${i}`;
-      body.classList.add("bg");
-    } else if (Number(input1.value) > number) {
-      i--;
-      high.textContent = "Too high!";
-      pl1score.textContent = `Player1-  ${i} `;
-
-      setTimeout(() => {
-        let number2 = Math.floor(Math.random() * 5) + 1;
-        input2.textContent = number2;
-        if (number2 === number) {
-          high.textContent = "Player2 Wins!";
-          highscore.textContent = `High score- ${i1}`;
-          question.textContent = number;
-          body.classList.add("bg");
-        } else if (number2 > number) {
-          high.textContent = "Too high!";
-          i1--;
-          pl2score.textContent = `Player2- ${i1}`;
-        } else {
-          high.textContent = "Too low!";
-          i1--;
-          pl2score.textContent = `Player2- ${i1}`;
-        }
-      }, 2000);
-    } else {
-      i--;
-      high.textContent = "Too low!";
-      pl1score.textContent = `Player1-  ${i}`;
-      setTimeout(() => {
-        let number2 = Math.floor(Math.random() * 5) + 1;
-        input2.textContent = number2;
-        if (number2 === number) {
-          high.textContent = "Player2 Wins!";
-          highscore.textContent = `High score- ${i1}`;
-          question.textContent = number;
-          body.classList.add("bg");
-        } else if (number2 > number) {
-          high.textContent = "Too high!";
-          i1--;
-          pl2score.textContent = `Player2- ${i1}`;
-        } else {
-          high.textContent = "Too low!";
-          i1--;
-          pl2score.textContent = `Player2- ${i1}`;
-        }
-      }, 2000);
-    }
-  });
 });
